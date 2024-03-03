bf6cfdd524ba608de900d31fb697e9732e50a942
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-03-03 13:08:24 -0800

    added style to employee list and it's items

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,9 +1,14 @@
 import EmployeeListItem from "../EmployeeListItem/EmployeeListItem";
 import employees from '../../data/employees';
+import './EmployeeList.css'
 
 export default function EmployeeList() {
 
-    const populatedList = employees.map((el)=>{ return <EmployeeListItem name={el.name} key={el.id} job={el.job} img={el.img}/>})
+    const populatedList = employees.map((el)=>{ 
+        let isActive = false;
+        if(el.img == "King") isActive = true;
+
+        return <EmployeeListItem name={el.name} key={el.id} job={el.job} img={el.img} isActive={isActive}/>})
 
     return (
         <>
--- src/components/EmployeeListItem/EmployeeListItem.jsx
+++ src/components/EmployeeListItem/EmployeeListItem.jsx
@@ -4,25 +4,31 @@ import lee from '../../images/Lee.png'
 import monroe from '../../images/Monroe.png'
 import taylor from '../../images/Taylor.png'
 import williams from '../../images/Williams.png'
+import './EmployeeListItem.css'
 
 const employeePhotos = {
-    "King" : king,
-"Jones": jones,
-"Lee": lee,
-"Monroe": monroe,
-"Taylor": taylor,
-"Williams": williams
+    "King": king,
+    "Jones": jones,
+    "Lee": lee,
+    "Monroe": monroe,
+    "Taylor": taylor,
+    "Williams": williams
 };
 
 
-export default function EmployeeListItem({key,job,name, img}){
-    return(
+export default function EmployeeListItem({ job, name, img, isActive }) {
+    let className = "photo";
+    if(isActive) className += " active";
+     
+    return (
         <>
-        <li key={key}>
-        <div className="pic"><img src={employeePhotos[img]} alt={name}/></div>
-                <h4>{name}</h4>
-                <p>{job}</p>
-        </li>
+            <li className="employee-list-card" >
+                <div className={className} ><img src={employeePhotos[img]} alt={name} /></div>
+                    <div className="info">
+                        <h4>{name}</h4>
+                        <p>{job}</p>
+                    </div>
+            </li>
         </>
     );
 }
